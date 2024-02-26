8e1ef36ae22c927f552465d8754d38c15251a693
Aaron Evans <aarone@outlook.com.au>
2024-02-27 10:26:07 +1100

    github_username variable

--- variables.tf
+++ variables.tf
@@ -2,3 +2,8 @@ variable "tfc_organization_name" {
   type        = string
   description = "The name of the Terraform Cloud organization."
 }
+
+variable "github_username" {
+  type        = string
+  description = "The name of your GitHub Username."
+}
