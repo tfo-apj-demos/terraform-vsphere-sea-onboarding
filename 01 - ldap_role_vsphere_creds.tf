e9bab147d4cceaf60c8308372e0dd59ddcc02986
Aaron Evans <aarone@outlook.com.au>
2024-07-31 12:47:45 +1000

    init

---
+++ 01 - ldap_role_vsphere_creds.tf
@@ -0,0 +1,11 @@
+# Usage: This file is used to read the ldap role credentials from Vault for vSphere and NSX providers.
+
+data "vault_ldap_static_credentials" "vm_builder" {
+  mount     = "ldap"
+  role_name = "vm_builder"
+}
+
+data "vault_ldap_static_credentials" "nsx_read_only" {
+  mount     = "ldap"
+  role_name = "nsx_read_only"
+}
