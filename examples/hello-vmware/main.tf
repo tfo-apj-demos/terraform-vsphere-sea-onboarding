7c8cc98899d20a867e5bfd68d78ea63dcec76975
Aaron Evans <aarone@outlook.com.au>
2024-08-21 13:50:01 +1000

    updated 1.0.4

--- examples/hello-vmware/main.tf
+++ examples/hello-vmware/main.tf
@@ -32,7 +32,7 @@ module "ssh_role" {
 
 # --- Create Boundary targets for the TFC Agent
 module "boundary_target" {
-  source = "github.com/tfo-apj-demos/terraform-boundary-target?ref=1.0.3"
+  source = "github.com/tfo-apj-demos/terraform-boundary-target?ref=1.0.4"
 
   hosts = [
     {
