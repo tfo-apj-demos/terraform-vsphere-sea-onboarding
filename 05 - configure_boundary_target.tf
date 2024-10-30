dc60001f86637341bc982a25193ac9edc10aaf8e
Aaron Evans <aarone@outlook.com.au>
2024-10-30 23:58:50 +1100

    comment out SSH role and TFC agent target module configurations in configure_boundary_target.tf

--- 05 - configure_boundary_target.tf
+++ 05 - configure_boundary_target.tf
@@ -1,25 +1,25 @@
-module "ssh_role" {
-  source = "github.com/tfo-apj-demos/terraform-vault-ssh-role?ref=1.0.0"
+# module "ssh_role" {
+#   source = "github.com/tfo-apj-demos/terraform-vault-ssh-role?ref=1.0.0"
 
-  ssh_role_name = "${var.github_username}-gcve-terraform-cloud-agent"
-}
+#   ssh_role_name = "${var.github_username}-gcve-terraform-cloud-agent"
+# }
 
-module "tfc_agent_target" {
-  source               = "github.com/tfo-apj-demos/terraform-boundary-target-refactored"
+# module "tfc_agent_target" {
+#   source               = "github.com/tfo-apj-demos/terraform-boundary-target-refactored"
 
-  project_name         = var.github_username
-  target_name          = "gcve-tfc-agent"
-  hosts                = ["${module.tfc-agent.virtual_machine_name}.hashicorp.local"]
-  port                 = 22
-  target_type          = "ssh"
+#   project_name         = var.github_username
+#   target_name          = "gcve-tfc-agent"
+#   hosts                = ["${module.tfc-agent.virtual_machine_name}.hashicorp.local"]
+#   port                 = 22
+#   target_type          = "ssh"
 
-  # Vault credential configurations
-  use_credentials      = true
-  credential_store_token = module.ssh_role.token
-  vault_address        = var.vault_address
-  credential_source    = "vault"
-  credential_path      = module.ssh_role.credential_path
+#   # Vault credential configurations
+#   use_credentials      = true
+#   credential_store_token = module.ssh_role.token
+#   vault_address        = var.vault_address
+#   credential_source    = "vault"
+#   credential_path      = module.ssh_role.credential_path
 
-  # Optional alias
-  alias_name           = "${module.tfc-agent.virtual_machine_name}.hashicorp.local"
-}
+#   # Optional alias
+#   alias_name           = "${module.tfc-agent.virtual_machine_name}.hashicorp.local"
+# }
